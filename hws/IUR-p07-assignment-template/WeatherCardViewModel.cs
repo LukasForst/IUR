396917216eeb46e6c30b06df1f82ce46e348615a
Lukas Forst <lukas.forst@blindspot.ai>
2018-11-20 14:51:22 +0100

    add template for last assignment

---
+++ hws/IUR-p07-assignment-template/WeatherCardViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IUR_p07
+{
+    /// <summary>
+    /// Class representing simplified (without notification) view model 
+    /// </summary>
+    public class WeatherCardViewModel
+    {
+        public string Location { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public string Conditions { get; set; }
+        public string IconPath { get; set; }
+    }
+}
---
+++ hws/IUR-p07-assignment-template/WeatherCustomControl.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace IUR_p07
+{
+    public class WeatherCustomControl : ToggleButton
+    {
+        static WeatherCustomControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WeatherCustomControl), new FrameworkPropertyMetadata(typeof(WeatherCustomControl)));
+        }
+
+        /*
+         * FINALIZE CUSTOM CONTROL
+         */
+    }
+
+    public class BoolToVisibiltyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool parameterBool = bool.Parse(parameter.ToString());
+
+            if ((bool)value != parameterBool)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class WeatherAPIImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var image = new Image();
+            var fullFilePath = String.Format("http://openweathermap.org/img/w/{0}.png", value);
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(fullFilePath, UriKind.Absolute);
+            bitmap.EndInit();
+
+
+            return bitmap;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
